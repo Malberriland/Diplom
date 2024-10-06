c41dac2738634c06f47b4f12854000362dda4557
Malberriland <164664415+Malberriland@users.noreply.github.com>
2024-10-07 02:00:44 +0300

    Update px-line.py

--- px-line.py
+++ px-line.py
@@ -5,32 +5,33 @@ from dash import Dash, dcc, html
 
 app = Dash(__name__)
 
-df = pd.read_csv('ohlc.csv')
-
-fig = px.line(df, x='Date', y='Close', height=650)
-
-fig.update_layout(
-    yaxis_title='Цена закрытия',
-    xaxis_title='Дата',
-    font=dict(family="Times New Roman", size=20, color="Black"),
-)
-fig.update_xaxes(rangeslider_visible=True, rangeselector=dict(
-    buttons=list([
-            dict(count=1, label="1m", step="month", stepmode="backward"),
-            dict(count=6, label="6m", step="month", stepmode="backward"),
-            dict(step="all")
-        ])
+def linegraph(df_):
+    """Return a line graph."""
+    df = pd.read_csv(df_)
+    fig = px.line(df, x='Date', y='Close', height=650)
+    fig.update_layout(
+        yaxis_title='Цена, USD',
+        xaxis_title='Дата',
+        font=dict(family="Times New Roman", size=20, color="Black"),
     )
-)
+    fig.update_xaxes(rangeslider_visible=True, rangeselector=dict(
+        buttons=list([
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
+    return fig
 
 
 app.layout = html.Div(
         children=[
             html.H1('Динамика цены'),
             dcc.Graph(
-                figure=fig
+                figure=linegraph('ohlc.csv')
                     )
         ]
     )
 
-app.run_server()
+app.run_server(debug=True)
