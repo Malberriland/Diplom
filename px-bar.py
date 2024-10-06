14c3ec7a213da1479dfb15b1365f59e4d7d89af1
Malberriland <164664415+Malberriland@users.noreply.github.com>
2024-10-07 02:01:09 +0300

    Update px-bar.py

--- px-bar.py
+++ px-bar.py
@@ -4,28 +4,33 @@ from dash import Dash, dcc, html
 
 
 app = Dash(__name__)
-df = pd.read_csv('budget_data.csv')
-df.head()
-fig = go.Figure(
-    data=[
-        go.Bar(x=df['Date'], y=df['Profit/Losses'], marker_color='lightskyblue')
-    ], layout=dict(height=650)
-)
 
-fig.update_layout(
-                  yaxis_title='Прибыль / Убытки, тыс.',
-                  xaxis_title='',
-                  font=dict(family="Times New Roman", size=20, color="Black")
-)
+def bargraph(df_):
+    """Return a bar graph."""
+    df = pd.read_csv(df_)
+    df.head()
+    fig = go.Figure(
+        data=[
+            go.Bar(x=df['Date'], y=df['Profit/Losses'], marker_color='lightskyblue')
+        ],
+        layout=dict(height=650)
+    )
+
+    fig.update_layout(
+                      yaxis_title='Прибыль / Убытки, тыс.',
+                      xaxis_title='',
+                      font=dict(family="Times New Roman", size=20, color="Black")
+    )
+    return fig
+
 
 app.layout = html.Div(
         children=[
             html.H1('Объем торговли с течением времени'),
             dcc.Graph(
-                figure=fig
+                figure=bargraph('budget_data.csv')
                     )
         ]
     )
 
 app.run_server(debug=True)
-
